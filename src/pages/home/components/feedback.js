6a069f4767065d25857074045f59ee4d96621911
SoorajModi <soomodi@gmail.com>
2022-04-06 21:33:27 -0400

    Fix subheader content formatting

--- src/pages/home/components/feedback.js
+++ src/pages/home/components/feedback.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Image from '../../../assets/CiteBot.png';
 
 function Feedback() {
   return (
-    <div className="flex flex-col space-y-3 w-full">
-      <span className="text-4xl font-light">Feedback</span>
-      <span className="text-lg text-gray-400">Get immediate feedback while building your citation across all edge cases.</span>
+    <div className="flex w-full justify-center items-center space-x-36">
+      <div className="flex flex-col space-y-3">
+        <span className="text-4xl font-light">Feedback</span>
+        <span className="text-lg text-gray-400 w-60">Get immediate feedback while building your citation across all edge cases.</span>
+      </div>
+      <div className="flex">
+        <img src={Image} alt="demo of live feedback" className="w-96" />
+      </div>
     </div>
   );
 }
--- src/pages/home/components/project.js
+++ src/pages/home/components/project.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Image from '../../../assets/CiteBot.png';
 
 function Project() {
   return (
-    <div className="flex flex-col space-y-3 w-full">
-      <span className="text-4xl font-light">Project</span>
-      <span className="text-lg text-gray-400">Save citations to projects and edit them in the future. Never lose a citation again!</span>
+    <div className="flex w-full justify-center items-center space-x-36">
+      <div className="flex">
+        <img src={Image} alt="demo of live feedback" className="w-96" />
+      </div>
+      <div className="flex flex-col space-y-3">
+        <span className="text-4xl font-light">Project</span>
+        <span className="text-lg text-gray-400 w-60">Save citations to projects and edit them in the future. Never lose a citation again!</span>
+      </div>
     </div>
   );
 }
